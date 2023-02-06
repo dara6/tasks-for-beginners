775741a351544f899e8ffb70c6b89f690d90ef9a
daraa <daraabbb@icloud.com>
2023-02-06 10:27:47 +0300

    wip

---
+++ 5. String Exercise/6.py
@@ -0,0 +1,12 @@
+def func(str1, str2):
+    result = ''
+    reversed_str2 = str2[::-1]
+    for i in range(0, len(str1)):
+        result += str1[i] + reversed_str2[i]
+    print(result)
+    return ''
+
+
+s1 = "Abc"
+s2 = "Xyz"
+print(func(s1, s2))
