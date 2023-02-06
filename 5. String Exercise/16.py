2457ad932b3d79a4e73d568a6c75c5b9bf66ecff
daraa <daraabbb@icloud.com>
2023-02-06 13:54:23 +0300

    remove symbols from string except integers

---
+++ 5. String Exercise/16.py
@@ -0,0 +1,10 @@
+def remove_ch_str(str):
+    res_str = ''
+    for s in str:
+        if s.isdigit():
+            res_str += s
+    return res_str
+
+
+str1 = 'I am 25 years and 10 months old'
+print(remove_ch_str(str1))
